50f02663f6bd0ae2426425f768cb6261ba9cf3bf
Decio Luiz <decioluiz@msn.com>
2023-01-10 17:13:51 -0300

    Inclusão de documentação no código (Classe Agenda) explicando os métodos

--- agenda/Agenda.java
+++ agenda/Agenda.java
@@ -6,15 +6,15 @@ import java.util.Scanner;
 public class Agenda {
 
     Scanner sc = new Scanner(System.in);
-
+    
     ArrayList<Pessoa> ag = new ArrayList<>();
     Pessoa p = new Pessoa();
 
     public Agenda() {
         super();
-
     }
 
+    //Método para adicionar novo objeto "Pessoa" (nome, idade, altura) na agenda
     public void armazenarPessoa() {
         p = new Pessoa();
         System.out.println("Digite o nome: ");
@@ -25,9 +25,11 @@ public class Agenda {
         p.setAltura((float) sc.nextDouble());
         ag.add(p);
     }
-
+    
+    /*Método para remover um registro da agenda, utilizando como parâmetro uma String (nome) e comparando se o valor
+        inserido pelo usuário é igual a algun dos registros, se True, o mesmo é removido.
+    */
     private String nomeDel;
-
     public void removerPessoa() {
         System.out.println("Digite o nome da pessoa que deseja remover: ");
         nomeDel = sc.next();
@@ -38,9 +40,11 @@ public class Agenda {
             }
         }
     }
-
+    
+    /*Método para buscar determinado registro na agenda, utilizando como parâmetro uma String (nome) e retornando
+        o resultado procurado.
+    */
     String nomeBusca;
-
     public int buscarPessoa() {
         System.out.println("Digite a nome da pessoa que deseja procurar: ");
         nomeBusca = sc.next();
@@ -52,12 +56,16 @@ public class Agenda {
         return 0;
     }
 
+    //Método para imprimir toda a agenda
     public void imprimirAgenda() {
         for (int i = 0; i < ag.size(); i++) {
             System.out.println(ag.get(i));
         }
     }
-
+    
+    /*Método para imprimir apenas um registro/objeto, utilizando como parâmetro um número inteiro representando
+        o índice do registro na lista
+    */
     public void imprimirPessoa() {
         System.out.println("Digite o número do índice que deseja imprimir: ");
         int index = sc.nextInt();
